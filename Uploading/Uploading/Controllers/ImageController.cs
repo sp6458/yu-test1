d17fc5287a36059c1c70d2cfde4a12bf5e34febb
sp6458 <sp645@hotmail.com>
2022-01-11 20:54:59 -0800

    upload image

---
+++ Uploading/Uploading/Controllers/ImageController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Uploading.Models;
+
+namespace Uploading.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Image imageModel)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+            string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            imageModel.ImagePath = "~/Image/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+            imageModel.ImageFile.SaveAs(fileName);
+            using (MvcImageDBEntities db = new MvcImageDBEntities())
+            {
+                db.Images.Add(imageModel);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult View(int id)
+        {
+            Image imageModel = new Image();
+
+            using (MvcImageDBEntities db = new MvcImageDBEntities())
+            {
+                imageModel = db.Images.Where(x => x.ImageID == id).FirstOrDefault();
+            }
+
+            return View(imageModel);
+        }
+    }
+}
